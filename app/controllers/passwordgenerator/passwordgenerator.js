a35c1b5bce80855e0cb34c7e5462fc908c6eab72
dpjb71 <dpjb78@gmail.com>
2016-05-16 19:52:21 +0200

    Standardize name of passwordgenerator. Give LAdmin name its real place.

--- app/controllers/passwordgenerator/passwordgenerator.js
+++ app/controllers/passwordgenerator/passwordgenerator.js
@@ -3,7 +3,7 @@ var ladminPassword = ladmin.createController(ladmin.index, 'ladmin.password')
 .oninit(function() {
     this.getScript('js/strongPasswordGenerator.js');
     $('#generatePassword').on('click', function() {
-        displayPassword();
+        ladminPassword.displayPassword();
     })    
 })
 .actions({
