5c2a91d32487c0036281ecb7f7778be0064ef5ea
david <dblanchard1@bbox.fr>
2016-11-01 14:04:12 +0100

    Ladmin.js
    Reorder code and add bindEvents method

--- js/ladmin.js
+++ js/ladmin.js
@@ -8,48 +8,51 @@ var ladmin = TWebApplication.create(host);
 ladmin.index = ladmin.createView('index');
 
 var ladminIndex = ladmin.createController(ladmin.index, 'ladmin.index')
-.onload(function() {
-    window.onkeydown = function(e) {    
-        var code = e.keyCode ? e.keyCode : e.which;
-
-        if(code === 27) { // ESC is typed
-            ladminIndex.word = '';
-            console.log("ladminIndex.word = '" + ladminIndex.word + "'");
-        }
-    };
-
-    window.onkeypress = function(e) {    
-        var code = e.keyCode ? e.keyCode : e.which;
-
-        if (code === 35 && ladminIndex.word === '') { // # is typed
-            ladminIndex.word += String.fromCharCode(code);
-            console.log("ladminIndex.word = '" + ladminIndex.word + "'");
-        } else if (code === 33 && ladminIndex.word === '#') { // ! is typed
-            ladminIndex.word += String.fromCharCode(code);
-            ladminIndex.isLoggingIn = true; // trying to log in as administrator
-            console.log("ladminIndex.word = '" + ladminIndex.word + "'");
-        } else if (ladminIndex.isLoggingIn) {
-            ladminIndex.word += String.fromCharCode(code);
-            console.log("ladminIndex.word = '" + ladminIndex.word + "'");
-        }
-
-        if (ladminIndex.word === '#!admin') { // User name is complete
-    //    	alert("Please enter password.");L
-            if($('#myCarousel') !== undefined) {
-                var myCarousel = $('#myCarousel');
-                myCarousel.carousel({
-                    interval: 0
-                });
-                myCarousel.on('slid.bs.carousel', function() {
-                    ladminIndex.attachWindow('http://' + host + '/login.html', '#adminContent');
-                })
-                myCarousel.carousel('next');
-
-            }
-        }
-    };
-})
 .actions({
     word : ''
     , isLoggingIn : false
+    , bindEvents : function () {
+        window.onkeydown = function(e) {    
+            var code = e.keyCode ? e.keyCode : e.which;
+
+            if(code === 27) { // ESC is typed
+                ladminIndex.word = '';
+                console.log("ladminIndex.word = '" + ladminIndex.word + "'");
+            }
+        };
+
+        window.onkeypress = function(e) {    
+            var code = e.keyCode ? e.keyCode : e.which;
+
+            if (code === 35 && ladminIndex.word === '') { // # is typed
+                ladminIndex.word += String.fromCharCode(code);
+                console.log("ladminIndex.word = '" + ladminIndex.word + "'");
+            } else if (code === 33 && ladminIndex.word === '#') { // ! is typed
+                ladminIndex.word += String.fromCharCode(code);
+                ladminIndex.isLoggingIn = true; // trying to log in as administrator
+                console.log("ladminIndex.word = '" + ladminIndex.word + "'");
+            } else if (ladminIndex.isLoggingIn) {
+                ladminIndex.word += String.fromCharCode(code);
+                console.log("ladminIndex.word = '" + ladminIndex.word + "'");
+            }
+
+            if (ladminIndex.word === '#!admin') { // User name is complete
+        //    	alert("Please enter password.");L
+                if($('#myCarousel') !== undefined) {
+                    var myCarousel = $('#myCarousel');
+                    myCarousel.carousel({
+                        interval: 0
+                    });
+                    myCarousel.on('slid.bs.carousel', function() {
+                        ladminIndex.attachWindow('http://' + host + '/login.html', '#adminContent');
+                    })
+                    myCarousel.carousel('next');
+
+                }
+            }
+        };
+    }
+})
+.onload(function() {
+    ladminIndex = this;
 });
